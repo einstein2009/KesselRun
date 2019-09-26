8e0eacf6a01832f752f88db3f9423dd585b09edc
Stephen Fisher <sfish323@gmail.com>
2019-09-26 18:38:59 -0400

    Huge Updates
    Updated HUD, Pause Menu, Main Menu
    Added Multiple scripts: Player Health, Enemy Attack, Game Over, Damage, etc
    Added a capsule collider and attack script to the current asteroid for testing purposes
    Damage reflects on HUD
    Score Manager works, reflects time alive

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static float score;
+
+
+    Text text;
+
+
+    void Awake()
+    {
+        text = GetComponent<Text>();
+        score = 0;
+    }
+
+
+    void Update()
+    {
+        score += Time.deltaTime;
+        text.text = score.ToString("F2");
+    }
+}
