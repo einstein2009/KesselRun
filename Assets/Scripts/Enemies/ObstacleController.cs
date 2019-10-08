5e7b3fa9f14831d1098fbb0ff0b918e298b9f14b
Stephen Fisher <sfish323@gmail.com>
2019-10-08 17:40:29 -0400

    Added in first working obstacle

---
+++ Assets/Scripts/Enemies/ObstacleController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    public int attackDamage = 200;
+
+    GameObject player;
+    PlayerHealth playerHealth;
+
+
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player)
+        {
+            Attack();
+        }
+    }
+
+    void Attack()
+    {
+        if (playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
+}
