ac461d08c9ada8a5f22c5bf3c44972b56635c2d9
Nick Ackors <nickaackors@gmail.com>
2019-10-10 17:55:10 -0500

    updates

--- Assets/Scripts/Managers/GameOverManager.cs
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 
 public class GameOverManager : MonoBehaviour
 {
-
     public void MainMenu()
     {
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("MainLevel", LoadSceneMode.Single);
     }
 
     public void Quit()
