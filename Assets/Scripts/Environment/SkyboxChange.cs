00e23f05f5b7b643327da1a7da13dab7de310128
Stephen Fisher <sfish323@gmail.com>
2019-10-17 15:27:40 -0400

    Added Skybox rotation and did some cleanup

--- Assets/Scripts/Environment/SkyboxChange.cs
+++ Assets/Scripts/Environment/SkyboxChange.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class SkyboxChange : MonoBehaviour
 {
     public Material[] skyboxes;
+    public float speedMultiplier = 5;
 
-
+    private void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speedMultiplier);
+    }
     public void ChangeMySkybox()
     {
         int x = Random.Range(0, skyboxes.Length - 1);
